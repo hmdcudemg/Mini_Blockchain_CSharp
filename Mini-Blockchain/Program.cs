af6558197c61338c930e33af9117476fe60ec900
David Mendez Guardado <demg@outlook.com>
2018-09-14 12:40:47 -0500

    Add project files.

---
+++ Mini-Blockchain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mini_Blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
